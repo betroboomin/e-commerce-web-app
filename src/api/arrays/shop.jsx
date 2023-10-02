3c8d5e8e578ee49ac9fc93be7aaeb9bb9e4e28c6
betroboomin <121827404+betroboomin@users.noreply.github.com>
2023-10-02 09:03:05 +0100

    monitor home progress

--- src/api/arrays/shop.jsx
+++ src/api/arrays/shop.jsx
@@ -15,8 +15,6 @@ import Vostro from "../../assets/laptops/vostroLaptop.avif"
 import alienware from "../../assets/laptops/alienwarelaptop.webp"
 import Gseries from "../../assets/laptops/gseriesgaminglaptop.avif"
 
-
-
 import allinone from "../../assets/laptops/allinonedesktops.avif"
 import xpsdesktops from "../../assets/laptops/xpsdesktops.avif"
 import optiplex from "../../assets/laptops/optiplex.avif"
@@ -25,6 +23,20 @@ import vostrodesktops from "../../assets/laptops/vostroDesktops.avif"
 import alienwaredesktops from "../../assets/laptops/Alienwaredesktops.avif"
 import precision from "../../assets/laptops/preccisionrack.avif"
 import desktopdeals from "../../assets/laptops/desktopDeals.avif"
+
+
+
+import allmonitors from "../../assets/monitors/allmonitor.avif"
+import alienwaremonitors from "../../assets/monitors/alienwaremonitor.avif"
+import ultrasharp from "../../assets/monitors/ultrasharp.avif"
+import curvedmonitor from "../../assets/monitors/curvedmonitors.avif"
+import highresmonitors from "../../assets/monitors/4kmonitors.avif"
+import proffesional from "../../assets/monitors/proffesional.avif"
+import monitordeals from "../../assets/monitors/monitordeals.avif"
+import monitoraccesories from "../../assets/monitors/monitor accessories.avif"
+import videoconfrencing from "../../assets/monitors/videoconferencing.avif"
+
+
 export const laptop = [
 
   {
@@ -303,74 +315,88 @@ export const data =
     ],
     monitors: [
       {
-        heading: "XPS Laptops & 2-in-1 PCs",
-        boldleft: "Creators & Creativity",
+        heading: "All Monitors",
+        boldleft: "See all. Do everything.",
         price: 599,
-        text: "Power your passions with premium, precision-crafted laptops with innovative features.",
-        btnName: "XPS",
-        image: image1,
-        link: "./laptops",
-        name: "XPS laptops",
+        text: "View our full selection of monitors with 4/6/8K, video conferencing, touch, curved, UltraSharp and much more.",
+        btnName: "All Monitors",
+        image: allmonitors,
+        link: "./monitors",
+        name: "All Monitors",
       },
       {
-        heading: "XPS Laptops & 2-in-1 PCs",
-        boldleft: "Creators & Creativity",
+        heading: "Alienware & Gaming Monitors",
+        boldleft: "For gaming at all levels",
         price: 599,
-        text: "Power your passions with premium, precision-crafted laptops with innovative features.",
-        btnName: "XPS",
-        image: image22,
-        link: "./laptops",
-        name: "XPS laptops",
+        text: "Performance-oriented gaming monitors with a fast response time, fast refresh rate (up to 360 Hz) and incredibly vibrant colors.",
+        btnName: "Gaming Monitors",
+        image: alienwaremonitors,
+        link: "./monitors",
+        name: "Alienware Monitors",
       },
       {
-        heading: "Latitude Laptops & 2-in-1 PCs",
-        boldleft: "Business Performance & Collaboration",
+        heading: "UltraSharp Monitors",
+        boldleft: "Infinite perspectives. Unlimited possibilities.",
         price: 609,
-        text: "Work from anywhere with the world’s most intelligent and secure business laptops sustainably designed for productivity on the go.",
-        btnName: "Latitude",
-        image: laptopandboy,
-        link: "./laptops",
-        name: "Latitude laptops",
+        text: "Top quality monitors made with innovative design and technology for brilliant colors and ultimate performance.",
+        btnName: "UltraSharp",
+        image: ultrasharp,
+        link: "./monitors",
+        name: "UltraSharp Monitors",
       },
       {
-        heading: "Inspiron Laptops & 2-in-1 PCs",
-        boldleft: "Versatile Performance",
-        price: 299,
-        text: "Create, consume and connect in style with versatile laptops and 2-in-1s.",
-        btnName: "Inspiron",
-        image: laptop360,
-        link: "./laptops",
-        name: "Inspiron laptops",
+        heading: "Curved Monitors",
+        boldleft: "For artists, gamers and multitaskers",
+        text: "Maximize immersive viewing experiences with monitors that reduce glare and distortion.",
+        btnName: "Curved",
+        image: curvedmonitor,
+        link: "./monitors",
+        name: "Curved Monitors",
       },
       {
-        heading: "Vostro Laptops",
-        boldleft: "Small Business Productivity",
-        price: 579,
-        text: "Power your small business with laptops built to deliver security, performance and enhanced video conferencing.",
-        btnName: "Vostro",
-        image: Vostro,
-        link: "./laptops",
-        name: "Vostro laptops",
+        heading: "4K & 8K Monitors",
+        boldleft: "The best monitors for streaming & entertainment",
+        text: "Discover every detail in exceptionally detailed colors and with up to 8x higher resolution than in Full HD.",
+        btnName: "4K & 8K",
+        image: highresmonitors,
+        link: "./monitors",
+        name: "4K & 8K Monitors",
       },
       {
-        heading: "Alienware Gaming Laptops",
-        boldleft: "Immersive Gaming Experiences",
-        price: "1,349.99",
-        text: "Experience high-performance gaming with iconic designs and intelligently engineered laptops.",
-        btnName: "Alienware",
-        image: alienware,
-        link: "./laptops",
-        name: "Alienware laptops",
+        heading: "Professional Monitors",
+        boldleft: "For workplace performance",
+        text: "Improve your work performance with these reliable, feature-packed monitors designed to boost productivity.",
+        btnName: "Professional",
+        image: proffesional,
+        link: "./monitors",
+        name: "Professional Monitors",
       },
       {
-        heading: "G Series Gaming Laptops",
-        boldleft: "Gaming Performance",
-        price: "849.99",
-        text: "Take your game to the next level with split-second responsiveness and dynamic gameplay.",
-        btnName: "G Series",
-        image: Gseries,
-        link: "./laptops",
-        name: "G Series laptops",
+        heading: "Video Conferencing Monitors",
+        boldleft: "For connection and collaboration",
+        text: "Experience world-class collaboration with monitors designed for teamwork anywhere.",
+        btnName: "Conferencing",
+        image: videoconfrencing,
+        link: "./monitors",
+        name: "Video Conferencing Monitors",
+      },
+      {
+        heading: "Monitor Deals",
+        boldleft: "Limited-time offers",
+        text: "Take advantage of deals on a wide range of monitors. Shop our exclusive deals and get the best for less.",
+        btnName: "Deals",
+        image: monitordeals,
+        link: "./monitors",
+        name: "Monitor Deals",
+      },
+      {
+        heading: "Monitor Accessories",
+        boldleft: "Speakers, stands, cables & much more",
+        text: "Work more efficiently with accessories designed to get the most out of your viewing experience.",
+        btnName: "Accessories",
+        image:monitoraccesories ,
+        link: "./monitors",
+        name: "Monitor Accessories",
       },
     ],
   }
